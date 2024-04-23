548718baecdd368021ab3e30c30d2f343250095b
gregory-lumina <gregorysantini@luminadex.com>
2024-04-23 07:08:45 -0400

    Font Lexend Changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
 import { title, subtitle } from "@/components/primitives";
+import { fontLexendBold } from "@/config/fonts";
 
 export default function Home() {
   return (
     <section className="flex flex-col items-center md:items-start justify-start gap-4 md:gap-20 py-8 md:py-10 text-center md:text-left">
       <div className="inline-block max-w-3xl justify-center">
         <h1 className={title()}>
-          Decentralized Trust, <strong>Amplified</strong>
+          Decentralized Trust,{" "}
+          <strong className={fontLexendBold.className}>Amplified</strong>
         </h1>
         <h2 className={subtitle({ class: "mt-16 max-w-xl" })}>
           Zeko is a{" "}
-          <strong>zero-knowledge, cross-chain scaling protocol</strong> built on
-          Mina and designed for developers shaping the future of finance, AI,
-          gaming, and the internet
+          <strong className={fontLexendBold.className}>
+            zero-knowledge, cross-chain scaling protocol
+          </strong>{" "}
+          built on Mina and designed for developers shaping the future of
+          finance, AI, gaming, and the internet
         </h2>
       </div>
     </section>
--- config/fonts.ts
+++ config/fonts.ts
@@ -2,4 +2,10 @@ import { Lexend } from "next/font/google";
 
 export const fontLexend = Lexend({
   subsets: ["latin"],
+  weight: "300",
+});
+
+export const fontLexendBold = Lexend({
+  subsets: ["latin"],
+  weight: "700",
 });
