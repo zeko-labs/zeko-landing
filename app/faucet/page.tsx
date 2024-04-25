601c4e891f58bdf08f3af408d406d0543bd66f4b
gregory-lumina <gregorysantini@luminadex.com>
2024-04-25 16:59:42 -0400

    Fixing Bugs

--- app/faucet/page.tsx
+++ app/faucet/page.tsx
@@ -23,10 +23,10 @@ export default function DocsPage() {
 
   const handleRequest = async () => {
     if (requesting) return;
-    // await checkAddress();
-    // if (doesExist) return;
-    // await getTotalLength();
-    // await addAddress(totalLength + 1);
+    await checkAddress();
+    if (doesExist) return;
+    await getTotalLength();
+    await addAddress(totalLength + 1);
     setRequesting(true);
     const responseTxt = await fas.request(address);
     setResponseTxt(responseTxt);
