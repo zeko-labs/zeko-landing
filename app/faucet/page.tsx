e0f38e18182084089bac1cca129aaeb368c89001
gregory-lumina <gregorysantini@luminadex.com>
2024-04-25 16:56:49 -0400

    CHecking Bugs-2

--- app/faucet/page.tsx
+++ app/faucet/page.tsx
@@ -62,7 +62,7 @@ export default function DocsPage() {
   };
 
   useEffect(() => {
-    //supaInstance = createSupaClient();
+    supaInstance = createSupaClient();
   }, []);
 
   const NetworkSelector = () => (
