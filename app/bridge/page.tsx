eac1ae8f49f0cf1f0d09a69ef23d0746f2270f0a
gregory-lumina <gregorysantini@luminadex.com>
2024-05-16 15:55:39 -0400

    Bridge UI Update

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/input";
+import { mina } from "@/lib/wallet";
+import { connect } from "@/lib/wallet";
 
 export default function Bridge() {
   const [amount, setAmount] = useState("");
@@ -11,6 +13,39 @@ export default function Bridge() {
   const handleSwitchDirection = () => {
     setDirectFlag(!directFlag);
   };
+
+  const [connected, setConnected] = useState(1);
+
+  const handleConnectWallet = () => {
+    connect()
+      .then(() => setConnected(2))
+      .catch((err) => console.log(err));
+  };
+
+  const handleBridge = () => {
+    setConnected(3);
+    setTimeout(() => {
+      setConnected(4);
+    }, 3000);
+  };
+  const handleClaim = () => {};
+
+  const handleSend = (status: Number) => {
+    switch (status) {
+      case 1:
+        handleConnectWallet();
+        break;
+      case 2:
+        handleBridge();
+        break;
+      case 3:
+        break;
+      default:
+        handleClaim();
+        break;
+    }
+  };
+
   return (
     <div>
       <div className="pb-6 flex flex-col items-center">
@@ -55,7 +90,23 @@ export default function Bridge() {
           </div>
         </div>
       </div>
-      <div>
+      <Button
+        color="primary"
+        className="w-full md:w-fit"
+        radius="sm"
+        onClick={() => {
+          handleSend(connected);
+        }}
+      >
+        {connected === 1
+          ? "Connect Wallet"
+          : connected === 2
+          ? "Bridge ➜"
+          : connected === 3
+          ? "Awaiting Confirmation..."
+          : "Claim"}
+      </Button>
+      <div className="mt-10">
         <div className="text-3xl">Transaction History</div>
         <div className="pt-5">Your transaction will appear here.</div>
         <Button color="primary" className="w-full md:w-fit mt-5" radius="sm">
