f685805f5ed4f3de2f035ba5a72ad23a80d9878d
gregory-lumina <gregorysantini@luminadex.com>
2024-05-22 19:58:54 -0400

    Added Downloadable Resources

--- app/zekoboost/page.tsx
+++ app/zekoboost/page.tsx
@@ -81,6 +81,15 @@ export default function BoostPage() {
           >
             Entry
           </Button>
+          <Button
+            radius="md"
+            className={`p-10 ${
+              subpage === 5 ? "bg-[#4873ff]" : "bg-[#7494ff]"
+            } text-2xl text-white`}
+            onClick={() => setSubPage(5)}
+          >
+            Resources
+          </Button>
         </div>
         <div>
           {subpage < 4 ? (
@@ -90,7 +99,7 @@ export default function BoostPage() {
               height={700}
               alt="Page"
             />
-          ) : (
+          ) : subpage === 4 ? (
             <iframe
               src="https://docs.google.com/forms/d/e/1FAIpQLSedPNSBQD_5SSS2ZcT0nk_woS5TCk1H4zd59G4EiGTrlM1ibA/viewform?embedded=true"
               width="640"
@@ -98,6 +107,63 @@ export default function BoostPage() {
             >
               Loading…
             </iframe>
+          ) : (
+            <div className="flex flex-col gap-10 items-center w-full">
+              <div className="flex justify-between gap-20">
+                <a href="/downloads/Zeko-Logo-Dark.png" download>
+                  <Image
+                    src="/downloads/Zeko-Logo-Dark.png"
+                    alt="zeko-logo-dark"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+                <a href="/downloads/Zeko-Logo-Light.png" download>
+                  <Image
+                    src="/downloads/Zeko-Logo-Light.png"
+                    alt="zeko-logo-light"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </div>
+              <div className="flex justify-between gap-20">
+                <a href="/downloads/gray.png" download>
+                  <Image
+                    src="/downloads/gray.png"
+                    alt="zeko-gray"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+                <a href="/downloads/gray.YPL.png" download>
+                  <Image
+                    src="/downloads/gray.YPL.png"
+                    alt="zeko-gray-dark"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </div>
+              <div className="flex justify-between gap-20">
+                <a href="/downloads/black.png" download>
+                  <Image
+                    src="/downloads/black.png"
+                    alt="zeko-black"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+                <a href="/downloads/black.YPL.png" download>
+                  <Image
+                    src="/downloads/black.YPL.png"
+                    alt="zeko-black-dark"
+                    width={200}
+                    height={200}
+                  />
+                </a>
+              </div>
+            </div>
           )}
         </div>
       </div>
