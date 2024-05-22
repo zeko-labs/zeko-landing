e2d5068d09dd4a2e9bee9290ade196a1ee6d0666
gregory-lumina <gregorysantini@luminadex.com>
2024-05-22 14:47:01 -0400

    Zeko Boost-1

---
+++ app/zekoboost/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import { title } from "@/components/primitives";
+import { Button } from "@nextui-org/button";
+import Image from "next/image";
+
+export default function BoostPage() {
+  const [subpage, setSubPage] = useState(0);
+  const subPageUrl = [
+    "./splash/1.png",
+    "./splash/2.jpg",
+    "./splash/3.png",
+    "./splash/4.jpg",
+  ];
+  return (
+    <div className="flex flex-col items-center gap-8 w-full mb-20">
+      <div>
+        <Image
+          src={"./splash/ZekoBoost.png"}
+          alt="Boost Image"
+          width={400}
+          height={200}
+        />
+      </div>
+      <div>
+        <h1 className={`${title()} w-full`}>Developer Contest Details</h1>
+      </div>
+      <div>
+        <Button
+          radius="full"
+          className="bg-gradient-to-r from-[#F2BB3E] to-[#FFE359] text-white p-10 text-5xl w-full"
+        >
+          Join Discord to Earn Prizes --&gt;
+          <Image
+            src={"./splash/discord.png"}
+            alt="Discord Image"
+            width={50}
+            height={50}
+          />
+        </Button>
+      </div>
+      <div className="flex flex-col gap-20 w-full items-center">
+        <div className="flex flex-row justify-between w-full">
+          <Button
+            radius="md"
+            className="p-10 bg-[#5980ff] text-2xl text-white"
+            onClick={() => setSubPage(1)}
+          >
+            Prizes
+          </Button>
+          <Button
+            radius="md"
+            className="p-10 bg-[#5980ff] text-2xl text-white"
+            onClick={() => setSubPage(2)}
+          >
+            Judges
+          </Button>
+          <Button
+            radius="md"
+            className="p-10 bg-[#5980ff] text-2xl text-white"
+            onClick={() => setSubPage(3)}
+          >
+            Scoring
+          </Button>
+          <Button
+            radius="md"
+            className="p-10 bg-[#5980ff] text-2xl text-white"
+            onClick={() => setSubPage(4)}
+          >
+            Entry
+          </Button>
+        </div>
+        <div>
+          {subpage < 4 ? (
+            <Image
+              src={subPageUrl[subpage]}
+              width={1000}
+              height={1000}
+              alt="Page"
+            />
+          ) : (
+            <iframe
+              src="https://docs.google.com/forms/d/e/1FAIpQLSedPNSBQD_5SSS2ZcT0nk_woS5TCk1H4zd59G4EiGTrlM1ibA/viewform?embedded=true"
+              width="640"
+              height="1922"
+            >
+              Loading…
+            </iframe>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
