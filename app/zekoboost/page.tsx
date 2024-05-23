e1ab778788914fce9dc38d25f2347babf65472fd
gregory-lumina <gregorysantini@luminadex.com>
2024-05-23 12:24:16 -0400

    Features Changed

--- app/zekoboost/page.tsx
+++ app/zekoboost/page.tsx
@@ -12,7 +12,7 @@ export default function BoostPage() {
     "./splash/1.png",
     "./splash/2.jpg",
     "./splash/3.png",
-    "./splash/4.jpg",
+    "./splash/4.png",
   ];
   return (
     <div className="flex flex-col items-center gap-8 w-full mb-20">
@@ -31,7 +31,7 @@ export default function BoostPage() {
         <Link href="https://discord.gg/vSDxqAchUY">
           <Button
             radius="full"
-            className="bg-gradient-to-r from-[#F2BB3E] to-[#FFE359] text-white p-10 text-5xl w-full"
+            className="bg-gradient-to-r from-[#F2BB3E] to-[#FFE359] text-white p-10 text-4xl w-full"
           >
             Join Discord to Earn Prizes --&gt;
             <Image
@@ -44,48 +44,48 @@ export default function BoostPage() {
         </Link>
       </div>
       <div className="flex flex-col gap-20 w-full items-center">
-        <div className="flex flex-row justify-between w-full">
+        <div className="flex flex-row justify-between w-full gap-5">
           <Button
             radius="md"
-            className={`p-10 ${
+            className={`p-7 ${
               subpage === 1 ? "bg-[#4873ff]" : "bg-[#7494ff]"
-            } text-2xl text-white`}
+            } text-xl text-white`}
             onClick={() => setSubPage(1)}
           >
             Prizes
           </Button>
           <Button
             radius="md"
-            className={`p-10 ${
+            className={`p-7 ${
               subpage === 2 ? "bg-[#4873ff]" : "bg-[#7494ff]"
-            } text-2xl text-white`}
+            } text-xl text-white`}
             onClick={() => setSubPage(2)}
           >
             Judges
           </Button>
           <Button
             radius="md"
-            className={`p-10 ${
+            className={`p-7 ${
               subpage === 3 ? "bg-[#4873ff]" : "bg-[#7494ff]"
-            } text-2xl text-white`}
+            } text-xl text-white`}
             onClick={() => setSubPage(3)}
           >
             Scoring
           </Button>
           <Button
             radius="md"
-            className={`p-10 ${
+            className={`p-7 ${
               subpage === 4 ? "bg-[#4873ff]" : "bg-[#7494ff]"
-            } text-2xl text-white`}
+            } text-xl text-white`}
             onClick={() => setSubPage(4)}
           >
             Entry
           </Button>
           <Button
             radius="md"
-            className={`p-10 ${
+            className={`p-7 ${
               subpage === 5 ? "bg-[#4873ff]" : "bg-[#7494ff]"
-            } text-2xl text-white`}
+            } text-xl text-white`}
             onClick={() => setSubPage(5)}
           >
             Resources
@@ -109,59 +109,68 @@ export default function BoostPage() {
             </iframe>
           ) : (
             <div className="flex flex-col gap-10 items-center w-full">
-              <div className="flex justify-between gap-20">
-                <a href="/downloads/Zeko-Logo-Dark.png" download>
-                  <Image
-                    src="/downloads/Zeko-Logo-Dark.png"
-                    alt="zeko-logo-dark"
-                    width={200}
-                    height={200}
-                  />
-                </a>
-                <a href="/downloads/Zeko-Logo-Light.png" download>
-                  <Image
-                    src="/downloads/Zeko-Logo-Light.png"
-                    alt="zeko-logo-light"
-                    width={200}
-                    height={200}
-                  />
-                </a>
+              <div className="flex flex-col gap-3 items-center">
+                <div className="text-black text-2xl">Zeko Logo</div>
+                <div className="flex justify-between gap-20">
+                  <a href="/downloads/Zeko-Logo-Dark.png" download>
+                    <Image
+                      src="/downloads/Zeko-Logo-Dark.png"
+                      alt="zeko-logo-dark"
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                  <a href="/downloads/Zeko-Logo-Light.png" download>
+                    <Image
+                      src="/downloads/Zeko-Logo-Light.png"
+                      alt="zeko-logo-light"
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                </div>
               </div>
-              <div className="flex justify-between gap-20">
-                <a href="/downloads/gray.png" download>
-                  <Image
-                    src="/downloads/gray.png"
-                    alt="zeko-gray"
-                    width={200}
-                    height={200}
-                  />
-                </a>
-                <a href="/downloads/gray.YPL.png" download>
-                  <Image
-                    src="/downloads/gray.YPL.png"
-                    alt="zeko-gray-dark"
-                    width={200}
-                    height={200}
-                  />
-                </a>
+              <div className="flex flex-col gap-3 items-center">
+                <div className="text-black text-2xl">Partnership Mashup</div>
+                <div className="flex justify-between gap-20">
+                  <a href="/downloads/gray.png" download>
+                    <Image
+                      src="/downloads/gray.png"
+                      alt="zeko-gray"
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                  <a href="/downloads/gray.YPL.png" download>
+                    <Image
+                      src="/downloads/gray.YPL.png"
+                      alt="zeko-gray-dark"
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                </div>
               </div>
-              <div className="flex justify-between gap-20">
-                <a href="/downloads/black.png" download>
-                  <Image
-                    src="/downloads/black.png"
-                    alt="zeko-black"
-                    width={200}
-                    height={200}
-                  />
-                </a>
-                <a href="/downloads/black.YPL.png" download>
-                  <Image
-                    src="/downloads/black.YPL.png"
-                    alt="zeko-black-dark"
-                    width={200}
-                    height={200}
-                  />
-                </a>
+              <div className="flex flex-col gap-3 items-center">
+                <div className="text-black text-2xl">Partnership Mashup</div>
+                <div className="flex justify-between gap-20">
+                  <a href="/downloads/black.png" download>
+                    <Image
+                      src="/downloads/black.png"
+                      alt="zeko-black"
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                  <a href="/downloads/black.YPL.png" download>
+                    <Image
+                      src="/downloads/black.YPL.png"
+                      alt="zeko-black-dark"
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           )}
